8a3a3cc6b9e388ca5c0e0ffcfd67aec96a6bb530
Ke Wang <kewang@microsoft.com>
2017-05-07 00:19:34 -0700

    add SIGS-dev-app

--- SIGS-dev-app/SIGS-dev-app.py
+++ SIGS-dev-app/SIGS-dev-app.py
@@ -1,29 +1,9 @@
-from flask_httpauth import HTTPTokenAuth
-from flask import Flask, jsonify, url_for, make_response, request, redirect
-from auth_helper import get_token_on_behalf, get_token_refresh, get_signin_url, validate_token, get_user_token
-from graph_service import call_me_endpoint, call_send_mail_endpoint, call_messages_endpoint
-from token_cache import TokenCache
+from flask import Flask, url_for, request, redirect
+from auth_helper import get_signin_url, get_user_token
 import json
 import jwt
 
 app = Flask(__name__)
-tokens = TokenCache()
-auth = HTTPTokenAuth()
-
-
-@auth.verify_token
-def verify_token(token):
-    if 'Authorization' not in request.headers:
-        # Unauthorized
-        print 'No Authorization in header'
-        return False
-    else:
-        authz = request.headers.get('Authorization');
-        parts = authz.split(' ')
-        if len(parts) != 2 or parts[0].lower() != 'bearer':
-            print 'OAuth is required'
-            return False
-        return validate_token(parts[1])
 
 
 @app.route('/')
@@ -44,63 +24,6 @@ def hello():
     print "ACCESS_TOKEN"
     print json.dumps(decode, indent=4)
     return 'Hello!\n' + access_token
-
-
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
-
-
-@app.route('/sendmail/<string:email_address>', methods=['POST'])
-@auth.login_required
-def send_mail(email_address):
-    if not tokens.contains(email_address):
-        user_token = request.headers.get('Authorization')[7:]
-        token_response = get_token_on_behalf(user_token)
-        print json.dumps(token_response, indent=4)
-
-        access_token = token_response['access_token']
-        refresh_token = token_response['refresh_token']
-        user_info = call_me_endpoint(access_token)
-        print json.dumps(user_info, indent=4)
-        tokens.set(user_info['userPrincipalName'], refresh_token)
-    else:
-        cached_token = tokens.get(email_address)
-        print 'using cache:'
-        print json.dumps(cached_token, indent=4)
-
-        token_response = get_token_refresh(cached_token['refresh_token'])
-        print json.dumps(token_response, indent=4)
-        access_token = token_response['access_token']
-
-        user_info = call_me_endpoint(access_token)
-        print json.dumps(user_info, indent=4)
-
-    result = call_send_mail_endpoint(access_token, str(user_info), email_address)
-    print json.dumps(result, indent=4)
-
-    return make_response(result['details'], result['status'])
-
-
-@app.route('/messages/<string:user_id>', methods=['GET'])
-@auth.login_required
-def get_mail(user_id):
-    if not tokens.contains(user_id):
-        # Unauthorized
-        return make_response('No token available', 401)
-
-    cached_token = tokens.get(user_id)
-    print 'using cache:'
-    print json.dumps(cached_token, indent=4)
-
-    token_response = get_token_refresh(cached_token['refresh_token'])
-    print json.dumps(token_response, indent=4)
-    access_token = token_response['access_token']
-
-    result = call_messages_endpoint(access_token)
-    print json.dumps(result, indent=4)
-
-    return make_response(jsonify(result['details']), result['status'])
 
 
 if __name__ == '__main__':
--- SIGS-dev-app/auth_helper.py
+++ SIGS-dev-app/auth_helper.py
@@ -2,8 +2,6 @@ from urllib import urlencode
 import requests
 from config import client_id, client_secret
 import json
-import jwt
-from datetime import datetime
 
 # The OAuth authority.
 authority = 'https://login.microsoftonline.com'
@@ -13,27 +11,6 @@ authorize_url = '{0}{1}'.format(authority, '/common/oauth2/authorize?{0}')
 
 # The token issuing endpoint.
 token_url = '{0}{1}'.format(authority, '/common/oauth2/token')
-
-
-def validate_token(oauth_token):
-    decoded = jwt.decode(oauth_token, verify=False)
-    print "claims:", json.dumps(decoded, indent=4)
-
-    iss = decoded['iss']
-    if iss != "https://sts.windows.net/72f988bf-86f1-41af-91ab-2d7cd011db47/":
-        print "unknown issuer - {0}".format(iss)
-        return False
-
-    aud = decoded['aud']
-    if aud != "http://localhost:5000/1/9646111a-31f7-11e6-a9a7-0f220aab2a15":
-        print "unknown audience - {0}".format(aud)
-        return False
-
-    exp = decoded['exp']
-    valid = datetime.now() <= datetime.fromtimestamp(exp)
-    if not valid:
-        print "token expired"
-    return valid
 
 
 def get_signin_url(redirect_uri):
@@ -67,42 +44,4 @@ def get_user_token(redirect_uri, code):
     except:
         return 'Error retrieving token: {0} - {1}'.format(r.status_code, r.text)
 
-
-def get_token_on_behalf(user_token):
-    # Build the post form for the token request
-    post_data = {'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
-                 'assertion': user_token,
-                 'client_id': client_id,
-                 'client_secret': client_secret,
-                 'resource': 'https://graph.microsoft.com',
-                 'requested_token_use': 'on_behalf_of'
-                 }
-
-    r = requests.post(token_url, data=post_data)
-    print "POST {0}".format(token_url)
-    print json.dumps(post_data, indent=4)
-
-    try:
-        return r.json()
-    except:
-        return 'Error retrieving token: {0} - {1}'.format(r.status_code, r.text)
-
-
-def get_token_refresh(refresh_token):
-    # Build the post form for the token request
-    post_data = {'grant_type': 'refresh_token',
-                 'refresh_token': refresh_token,
-                 'client_id': client_id,
-                 'client_secret': client_secret,
-                 'resource': 'https://graph.microsoft.com',
-                 }
-
-    r = requests.post(token_url, data=post_data)
-    print "POST {0}".format(token_url)
-    print json.dumps(post_data, indent=4)
-
-    try:
-        return r.json()
-    except:
-        return 'Error retrieving token: {0} - {1}'.format(r.status_code, r.text)
 
